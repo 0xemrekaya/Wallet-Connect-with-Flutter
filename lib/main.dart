2c1c76da21f1555af881711977621d1f6f1b24af
Emre Kaya <emrekaya75@outlook.com>
2023-09-06 00:09:08 +0300

    connected service

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
 import 'package:walletconnect_modal_flutter/walletconnect_modal_flutter.dart';
+import 'screens/my_home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -27,80 +27,3 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late WalletConnectModalService service;
-
-  @override
-  void initState() {
-    super.initState();
-    serviceInit();
-  }
-
-  Future<void> serviceInit() async {
-    service = WalletConnectModalService(
-      projectId: "932bfa5375a38d53adda143fbb6486f0",
-      metadata: const PairingMetadata(
-        name: 'wCon',
-        description: 'Flutter WalletConnectModal Sign Example',
-        url: 'https://walletconnect.com/',
-        icons: ['https://walletconnect.com/walletconnect-logo.png'],
-        redirect: Redirect(
-          native: 'flutterdapp://',
-          universal: 'https://www.walletconnect.com',
-        ),
-      ),
-    );
-    await service.init();
-  }
-
-  Future<void> _connect() async {
-    if (!service.isInitialized) {
-      // Service must be initialized before calling this method.
-      return;
-    }
-
-    try {
-      await service.open(context: context);
-      print(service.address);
-
-      print(service.connectResponse);
-    } on TimeoutException {
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  await _connect();
-                },
-                child: Text("connect")),
-            Text(service.address.toString()),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                },
-                child: Text("set steate")),
-            WalletConnectModalConnect(service: service),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/screens/my_home_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
+import 'package:walletconnect_modal_flutter/walletconnect_modal_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WalletConnectModalService service;
+  bool isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    serviceInit();
+  }
+
+  Future<void> serviceInit() async {
+    service = WalletConnectModalService(
+      projectId: "932bfa5375a38d53adda143fbb6486f0",
+      metadata: const PairingMetadata(
+        name: 'wCon',
+        description: 'Flutter WalletConnectModal Sign Example',
+        url: 'https://walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'flutterdapp://',
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+    await service.init();
+  }
+
+  Future<void> _connect() async {
+    if (!service.isInitialized) {
+      // Service must be initialized before calling this method.
+      return;
+    }
+    try {
+      await service.open(context: context);
+      setState(() {
+        isConnected = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 100),
+              isConnected == true
+                  ? ElevatedButton(
+                      onPressed: () {
+                        service.disconnect();
+                        service.close();
+                        setState(() {
+                          isConnected = false;
+                        });
+                      },
+                      child: Text("Disconnect Wallet", style: Theme.of(context).textTheme.labelLarge ))
+                  :
+              ElevatedButton(
+                  onPressed: () async {
+                    await _connect();
+                  },
+                  child: Text("Connect Wallet", style: Theme.of(context).textTheme.labelLarge )),
+                  const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("Connected: ${service.address} ${service.isOpen}"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
